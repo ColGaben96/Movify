b065ca1fe78f15dd10c7d7746226666117fefba8
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-01-25 13:26:22 -0500

    Removed Vue HelloWorld and added NodeJS Backend

---
+++ movify/src/Movify.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">
+        <h1 style="text-align: center; color: white;">Movify</h1>
+      </b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
